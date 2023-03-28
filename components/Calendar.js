51b467d70f5d5fe9be9d75fef86e74a98694a3c4
Sander van Riet <r0903044@student.thomasmore.be>
2023-03-28 12:31:37 +0200

    extra data toegevoegd kalender

--- components/Calendar.js
+++ components/Calendar.js
@@ -6,7 +6,13 @@ export function MyCalendar() {
     const [events, setEvents] = useState({
         '2023-03-26': [{time: '19:30', event: 'The Shawshank Redemption'}],
         '2023-03-27': [{time: '20:00', event: 'The Godfather'}, {time: '21:00', event: 'The Godfather2'}],
-        '2023-03-28': [{time: '21:00', event: 'The Godfather'}],
+        '2023-03-28': [{time: '21:00', event: 'The Godfather: Part 2'},{time: '21:30', event: 'The Dark Knight'}],
+        '2023-03-29': [{time: '20:00', event: '12 angry men'},{time: '21:30', event: 'Inception'},{time: '22:30', event: 'Django Unchained'}],
+        '2023-03-30': [{time: '20:00', event: 'The Lord of the Rings: the return of the King'},{time: '20:30', event: 'Avatar'}],
+        '2023-03-31': [{time: '20:00', event: 'Transformers'},{time: '21:30', event: 'The Matrix'},{time: '22:30', event: 'Terminator'}],
+        '2023-04-01': [{time: '20:00', event: 'The Shawshank Redemption'},{time: '20:30', event: 'Inception'}],
+        '2023-04-02': [{time: '20:00', event: 'The Dark Knight'},{time: '20:30', event: 'The Godfather'}],
+
     });
     const [selectedDate, setSelectedDate] = useState('');
     const [newTime, setNewTime] = useState('');
